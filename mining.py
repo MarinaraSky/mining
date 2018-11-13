7209175d5a70b53f4c857310d9b540623b61c741
Jack Spence <jack.c.spence@gmail.com>
2018-11-13 10:26:04 -0500

    Added some base logic to keep track of deploying units and verification that landing zone is clear.

--- mining.py
+++ mining.py
@@ -2,6 +2,11 @@ import random
 import tkinter
 
 class Zerg:
+    returns = list()
+    starting_locations = dict()
+    landing_clear = dict()
+    deploying = dict()
+
     def __init__(self, health):
         self.health = health
 
@@ -14,30 +19,62 @@ class Overlord(Zerg):
         self.dashboard = dashboard
         self.maps = {}
         self.drones = {}
+        self.deploy = list()
+        self.random_map_id = 0
+        self.return_count = 0
+        self.drop_zone_count = 0
         #TODO make logic to create zerg horde
         drone_count = int(refined_minerals / 9)
         for value in range(drone_count):
             z = Drone()
             self.drones[id(z)] = z
+            self.deploy.append(id(z))
 
     def add_map(self, map_id, summary):
         self.maps[map_id] = summary
 
     def action(self):
-        action_choice = random.randint(0, 3)  # both arguments are inclusive
-        random_drone_id = random.choice(list(self.drones.keys()))
-        random_map_id = random.choice(list(self.maps.keys()))
-        if action_choice == 0:
-            result = "NONE"
-        elif action_choice == 1:
-            result = 'RETURN {}'.format(random_drone_id)
+        print("Zergs", self.drones.keys())
+        print("Returns", Zerg.returns)
+        print("Deploy", self.deploy)
+        self.delete = []
+        print(Zerg.starting_locations)
+        for zerg in self.drones:
+            if self.drones[zerg].health <= 0:
+                self.delete.append(zerg)
+        for zerg in self.delete:
+            self.drones.pop(zerg)
+        if Zerg.returns:
+            returning = Zerg.returns.pop()
+            result = 'RETURN {}'.format(returning)
+            Zerg.landing_clear[self.drones[returning].map] = True
+            self.deploy.append(returning)
+            self.return_count += 1
+        elif self.deploy:
+            deploying = self.deploy.pop()
+            self.drones[deploying].map = self.random_map_id
+            if self.random_map_id not in Zerg.landing_clear:
+                Zerg.landing_clear[self.random_map_id] = True
+            if self.random_map_id not in Zerg.deploying:
+                Zerg.deploying[self.random_map_id] = False
+            if Zerg.landing_clear[self.random_map_id] == True:
+                result = 'DEPLOY {} {}'.format(deploying, self.random_map_id)
+                if self.random_map_id < 2:
+                    self.random_map_id += 1
+                else:
+                    self.random_map_id = 0
+                Zerg.deploying[self.random_map_id] = True
+            else:
+                self.deploy.append(deploying)
+                result = "NONE"
         else:
-            result = 'DEPLOY {} {}'.format(random_drone_id, random_map_id)
+            result = "NONE"
         self.dashboard.log.config(state=tkinter.NORMAL)
         self.dashboard.log.insert(tkinter.END, result)
         self.dashboard.log.insert(tkinter.END, "\n")
         self.dashboard.log.see(tkinter.END)
         self.dashboard.log.config(state=tkinter.DISABLED)
+        print("Return count: ", self.return_count)
         return result
 
 class Drone(Zerg):
@@ -46,50 +83,69 @@ class Drone(Zerg):
         self.moves = 1
         self.capacity = 10
         self.carry = 0
+        self.steps = 0
+        self.map = 0
 
     def action(self, context):
         directions = {0: 'NORTH', 1: 'SOUTH', 2: 'EAST', 3: 'WEST'}
         print(self.carry)
-        print(context.north, context.south, context.east, context.west)
-        if context.north == "*":
+        if self.steps == 0 and self.map not in Zerg.starting_locations:
+            Zerg.starting_locations[self.map] = "{}, {}".format(context.x, context.y)
+        '''
+        print("North: ", context.north)
+        print("South: ", context.south)
+        print("East: ", context.east)
+        print("West: ", context.west)
+        print("X: ", context.x)
+        print("Y: ", context.y)
+        '''
+        if Zerg.starting_locations[self.map] == "{}, {}".format(context.x, context.y):
+            Zerg.landing_clear[self.map] = False
+        else:
+            Zerg.landing_clear[self.map] = True
+        if context.north == "*" and self.carry < 10:
             self.carry += 1
             return directions.get(0)
-        elif context.south == "*":
+        elif context.south == "*" and self.carry < 10:
             self.carry += 1
             return directions.get(1)
-        elif context.east == "*":
+        elif context.east == "*" and self.carry < 10:
             self.carry += 1
             return directions.get(2)
-        elif context.east == "*":
+        elif context.east == "*" and self.carry < 10:
             self.carry += 1
             return directions.get(3)
         if context.north == "_" and self.carry > 5:
             self.carry = 0
+            self.steps += 1
+            Zerg.returns.append(id(self))
             return directions.get(0)
         elif context.south == "_" and self.carry > 5:
             self.carry = 0
+            self.steps += 1
+            Zerg.returns.append(id(self))
             return directions.get(1)
         elif context.east == "_" and self.carry > 5:
             self.carry = 0
+            self.steps += 1
+            Zerg.returns.append(id(self))
             return directions.get(2)
         elif context.east == "_" and self.carry > 5:
             self.carry = 0
+            self.steps += 1
+            Zerg.returns.append(id(self))
             return directions.get(3)
-        if context.north == "~":
-            return directions.get(1)
-        elif context.south == "~":
-            return directions.get(0)
-        elif context.east == "~":
-            return directions.get(3)
-        elif context.east == "~":
-            return directions.get(2)
         if context.north == "#":
+            self.steps += 1
             return directions.get(1)
         elif context.south == "#":
+            self.steps += 1
             return directions.get(0)
         elif context.east == "#":
+            self.steps += 1
             return directions.get(3)
         elif context.east == "#":
+            self.steps += 1
             return directions.get(2)
         random_choice = random.randint(0, 3)  # both arguments are inclusive
         return directions.get(random_choice, "CENTER")
